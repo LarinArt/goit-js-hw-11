fe29b0a452e6d30fde2b49783b90ba4321e6e0ca
Larin Artem <larin_art@ukr.net>
2022-05-19 22:23:51 +0300

    change code

--- src/js/fetch-img.js
+++ src/js/fetch-img.js
@@ -5,5 +5,5 @@ export default async function fetchImages(value, page) {
     const key = '27450205-8765e518dc84e5655ddf2d669';
     const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
-    return await axios.get(`${url}${filter}`).then(response => response.data);
+    return await axios.get(`${url}${filter}`).then(res => res.data);
 };
