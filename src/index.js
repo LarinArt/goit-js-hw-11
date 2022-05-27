0999ca46eb66c937308126cf87c3089bed1bf77e
Larin Artem <larin_art@ukr.net>
2022-05-27 23:18:08 +0300

    mentor4

--- src/index.js
+++ src/index.js
@@ -3,31 +3,19 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import throttle from 'lodash.throttle';
-import { fetchImgParams, searchImg } from './js/fetch-img';
+import { params, searchImg } from './js/fetch-img';
 import cardTpl from './templates/templates.hbs';
-import { refs } from './js/constants';
+import { refs } from './js/refs';
 import Notiflix from 'notiflix';
 
-function renderCardImage(arr) {
-    refs.gallery.insertAdjacentHTML('beforeend', cardTpl(arr));
-}
 
-async function generateMarkup() {
-    try {
-        const result = await searchImg();
-        const images = result.data.hits;
-        generateImagesMarkup(images);
-    } catch (error) {
-        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-    }
-}
-
-function generateImagesMarkup(images) {
+async function generateImagesMarkup() {
+    const result = await searchImg();
+    console.log(result);
+    const images = result?.data?.hits;
     renderCardImage(images);
 
     let lightbox = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
         captionDelay: 250,
     });
     lightbox.refresh();
@@ -39,39 +27,38 @@ function totalHits(total) {
     }
 }
 
-function onFormInput(e) {
-    fetchImgParams.page = 1;
-    fetchImgParams.q = e.target.value;
-}
 
-function onFormSubmit(e) {
+async function onFormSubmit(e) {
+    params.page = 1;
+    params.q = e.currentTarget.elements.searchQuery.value;
     refs.gallery.innerHTML = '';
     e.preventDefault();
-    generateMarkup();
+    generateImagesMarkup();
     searchImg().then(({ data }) => totalHits(data.total));
 }
 
 function loadMore() {
-    fetchImgParams.page += 1;
-    generateMarkup();
+    params.page += 1;
+    generateImagesMarkup();
 }
 
 function onObserver(entries) {
     entries.forEach(entry => {
-        if (entry.intersectionRatio && fetchImgParams.q !== '') {
+        if (entry.intersectionRatio && params.q !== '') {
             loadMore();
         }
     });
 };
 
 const options = {
-    rootMargin: '0px',
+    rootMargin: '400px',
     threshold: 1.0,
 };
 
 const observer = new IntersectionObserver(onObserver, options);
 observer.observe(refs.observe);
 
-refs.searchInput.addEventListener('input', onFormInput);
-
 refs.searchForm.addEventListener('submit', onFormSubmit);
+function renderCardImage(arr) {
+    refs.gallery.insertAdjacentHTML('beforeend', cardTpl(arr));
+}
--- src/js/fetch-img.js
+++ src/js/fetch-img.js
@@ -1,31 +1,34 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '27450205-8765e518dc84e5655ddf2d669';
 
-const customAxios = axios.create({
-    baseURL: URL,
-    params: {
-        key: API_KEY,
-    }
-});
-
-export const fetchImgParams = {
+export const params = {
     q: "",
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
     per_page: 40,
     page: 1,
+    key: API_KEY,
 };
-
-export const searchImg = async (params) => {
+export async function searchImg() {
     try {
-        const res = await customAxios.get('', { params });
+        const res = await axios.get(URL, { params });
         return res.json();
     } catch (error) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
 };
+
+// export const searchImg = async (params) => {
+//     try {
+//         const res = await customAxios.get('', { params });
+//         return res.json();
+//     } catch (error) {
+//         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//     }
+// };
+
+
